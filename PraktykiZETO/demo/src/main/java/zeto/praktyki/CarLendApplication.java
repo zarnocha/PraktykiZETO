a6cbe560c06e6749e61740d0e0de66d59f9f1ff9
167433 <100282502+167433@users.noreply.github.com>
2023-08-30 14:49:11 +0200

    Add files via upload
    Dobra na razie mam tyle, sorry ale ciężko mi idzie dziś jutro będzie lepiej

---
+++ PraktykiZETO/demo/src/main/java/zeto/praktyki/CarLendApplication.java
@@ -0,0 +1,13 @@
+package zeto.praktyki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLendApplication.class, args);
+	}
+
+}
