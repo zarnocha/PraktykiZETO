30dfeffe60a7774fc1ccd618d41585c05217e269
167433 <100282502+167433@users.noreply.github.com>
2023-09-04 09:29:27 +0200

    Update RentRepository.java

--- backend/car_rental/src/main/java/zeto/praktyki/Rent/RentRepository.java
+++ backend/car_rental/src/main/java/zeto/praktyki/Rent/RentRepository.java
@@ -5,5 +5,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RentRepository extends JpaRepository<Rent,Long>{
-
+    Page<Rent> findAllByStartTime(LocalDateTime startTime, Pageable pagable);
+    Page<Rent> findAllByEndTime(LocalDateTime endTime, Pageable pagable);
+    Page<Rent> findAllByStartTimeBetween(LocalDateTime startTime, LocalDateTime endTime, Pageable pagable);
+    Page<Rent> findAllByEndTimeBetween(LocalDateTime startTime, LocalDateTime endTime, Pageable pagable);
+    Page<Rent> findAllByStartTimeAndEndTimeBetween(LocalDateTime startTime, LocalDateTime endTime, Pageable pagable);
 }
