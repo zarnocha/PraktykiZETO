ea173f5256e07bff1cf865e32a73f32ec0f0a96c
zarnocha <zarnocha00@gmail.com>
2023-09-05 12:21:37 +0200

    Podłączono Tailwind do projektu. Zmieniono animację pobierania danych. Dodano przycisk do ponownej próby pobrania danych. Zmodyfikowano wyświetlanie aut na stronie głównej z `display: flex` na `grid`.
    TODO: modal z filtrowaniem; ostylowanie listy aut.

--- frontend/src/app/car-card/car-card.component.ts
+++ frontend/src/app/car-card/car-card.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'car-card',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+  ],
   templateUrl: './car-card.component.html',
   styleUrls: ['./car-card.component.sass'],
   encapsulation: ViewEncapsulation.None,
@@ -15,12 +22,4 @@ import { MatCardModule } from '@angular/material/card';
 export class CarCardComponent {
   @Input()
   carData!: api.CarEntity;
-  // imageSource: SafeResourceUrl | undefined;
-  // constructor(private sanitizer: DomSanitizer) {}
-
-  // ngOnInit() {
-  //   this.imageSource = this.sanitizer.bypassSecurityTrustResourceUrl(
-  //     `data:image/png;base64, ${this.carData.picture}`
-  //   );
-  // }
-}
+ }
